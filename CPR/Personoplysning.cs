d6f4832def9f0b90a9f8eb349d8e4cf2cf57f9f0
Eo-Le-LearnToHack <101599426+Eo-Le-LearnToHack@users.noreply.github.com>
2022-05-02 21:51:44 +0200

    Add files via upload
    .cs files didn't get uploaded. Retry upload.

---
+++ CPR/Personoplysning.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPR
+{
+    public class Personoplysning
+    {
+        private uint First6digits;
+        private uint Last4digits;
+        private string CPRnr;
+        private uint Fødselsår;
+
+        public void CPR_met (uint first6digits, uint last4digits)
+        {
+            First6digits = first6digits;
+            Last4digits = last4digits;
+            CPRnr = first6digits + "-" + last4digits;
+        }
+
+        public string Udskriv_CPR_met ()
+        {
+            return CPRnr;
+        }
+
+        public uint Udskriv_first6digits_met ()
+        {
+            return First6digits;
+        }
+
+        public uint Udskriv_last4digits_met ()
+        {
+            return Last4digits;
+        }
+
+        public uint Udskriv_fødselsår_met ()
+        {
+            Fødselsår = First6digits % 100;
+            switch (Last4digits)
+            {
+                case < 4000:
+                    Fødselsår += 1900;
+                    break;
+   
+                default:
+                    Fødselsår += 2000;
+                    break;
+
+            
+            }
+            return Fødselsår;
+        }
+    }
+}
---
+++ CPR/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CPR;
+
+Personoplysning person1 = new();
+
+person1.CPR_met(uint first6digits, uint last4digits); //Indtast dit CPRnr her. Kør programmet og den udregner dit fødselsår.
+Console.WriteLine(person1.Udskriv_fødselsår_met());
