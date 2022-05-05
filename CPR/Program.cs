e61b992d3f8086e46d5b1dcd8a5ba1db500f3847
Dan Nguyen <Dan.Nguyen@Rsyd.Dk>
2022-05-05 14:26:00 +0200

    Add empty cw after first cw.

--- CPR/Program.cs
+++ CPR/Program.cs
@@ -6,7 +6,7 @@ Console.Clear();
 Personoplysning person1 = new();
 Console.WriteLine("Indtast dit CPR-nr. ddmmyy-xxxx");
 person1.CPRnr = Console.ReadLine();
-Console.Clear();
+Console.WriteLine("");
 Console.WriteLine($"Dit fødselsår er {person1.Udskriv_fødselsår_met()}");
 Console.WriteLine("");
 Console.WriteLine("Tryk på ENTER for at prøve igen");
